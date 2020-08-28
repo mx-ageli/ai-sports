a95dead37ee12e562eb1d7886d5ba800f032435b
mengjiaxin <mengjiaxin_0403@sina.com>
2020-08-28 18:38:27 +0800

    添加部署脚本

--- redeploy.sh
+++ redeploy.sh
@@ -12,7 +12,20 @@ mvn clean install -Dmaven.test.skip=true
 #nohup java -agentlib:jdwp=transport=dt_socket,address=8100,server=y,suspend=n -jar test-web.jar > /root/logs/test.log &
 
 #杀死原来的java进程
-./kill.sh ai-sports
+jarName="ai-sports-1.0-SNAPSHOT.jar"
+
+count=$(ps -ef |grep ${jarName} |grep -v "grep" |wc -l)
+
+if [ ${count} -gt 0 ]; then
+        echo "已存在${count}个${jarName}程序在运行"
+        jarPid=$(ps x |grep ${jarName} | grep -v grep | awk '{print $1}')
+        echo ${jarPid}
+        kill -9 ${jarPid}
+        output=`echo "正在关闭${jarName}程序,进程id${jarPid}"`
+        echo ${output}
+else
+        echo "没有对应的程序在运行"
+fi
 
 nohup java -jar /home/AiSports/ai-sports/target/ai-sports-1.0-SNAPSHOT.jar&
 #监控日志
