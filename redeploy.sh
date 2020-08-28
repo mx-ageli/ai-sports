4dcf220c9fb02ac1bedf4fd509d87c8cd19ccb4b
mengjiaxin <mengjiaxin_0403@sina.com>
2020-08-28 18:29:19 +0800

    添加部署脚本

---
+++ redeploy.sh
@@ -0,0 +1,19 @@
+#进入代码文件夹，必须有git管理
+cd /home/AiSports/ai-sports
+#更新代码
+git pull
+#清理原来的jar包重新打包
+mvn clean install -Dmaven.test.skip=true
+#cd ~
+#删除原来的jar包
+#rm -rf ai-sports-1.0-SNAPSHOT.jar.jar
+#cp code/test/test-web/target/test-web.jar test-web.jar
+#后台运行
+#nohup java -agentlib:jdwp=transport=dt_socket,address=8100,server=y,suspend=n -jar test-web.jar > /root/logs/test.log &
+
+#杀死原来的java进程
+./kill.sh ai-sports-1.0-SNAPSHOT.jar
+
+nohup java -jar /home/AiSports/ai-sports/target/ai-sports-1.0-SNAPSHOT.jar&
+#监控日志
+tail -f /home/AiSports/ai-sports/nohup.out
